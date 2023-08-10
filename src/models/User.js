8eadb6757feec50dbf4fdc7fe491b741a004367a
Cristian <crissegu777@gmail.com>
2023-08-10 10:50:21 -0500

    first commit

---
+++ src/models/User.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../utils/connection'); 
+  
+const user = sequelize.define('user', {
+
+    first_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    last_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    birthday: {
+        type: DataTypes.STRING
+    }
+});
+
+module.exports = user;
